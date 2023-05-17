927bccf03ed50bdf00fa382fada01088a5feea16
nurik17 <91252221+nurik17@users.noreply.github.com>
2023-05-17 14:59:23 +0600

    adding_random_meal with Retrofit(using API)

---
+++ app/src/main/java/com/example/foodapp/retrofit/RetrofitInstance.kt
@@ -0,0 +1,19 @@
+package com.example.foodapp.retrofit
+
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.create
+
+object RetrofitInstance {
+    private const val BASE_URL ="https://www.themealdb.com/api/json/v1/1/"
+
+   val api:MealApi by lazy {
+       Retrofit.Builder()
+           .baseUrl(BASE_URL)
+           .addConverterFactory(GsonConverterFactory.create())
+           .build()
+           .create(MealApi::class.java)
+   }
+
+
+}
